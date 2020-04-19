355da74126e8ad6592ba1a73da938c2e39743dca
Marco Antonio <masjr@amazon.com>
2020-04-19 18:27:34 -0300

    ENH: fix logic

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def render(matrix):
     clear()
     for line in matrix:
         print(" ".join(line))
-    input('Enter for next gen')
+    sleep(0.5)
 
 
 def create_matrix(columns, lines, initial_population):
     matrix = [[" "] * columns for _ in range(lines)]
-    
+
     # randomize matrix
     for points in range(initial_population):
         column, line = (random.randrange(0, columns), random.randrange(0, lines))
@@ -35,9 +35,9 @@ def generate_new_matrix(matrix):
     line_length = len(matrix)
     columns_length = len(matrix[0])
     result_matrix = [[" "] * columns_length for _ in range(line_length)]
-    
+
     for line_index, columns in enumerate(matrix):
-        for column_index, value in enumerate(columns):    
+        for column_index, value in enumerate(columns):
             horizontal_neighbours = (
                 matrix[line_index][column_index - 1]
                 + matrix[line_index][(column_index + 1) % columns_length]
@@ -50,20 +50,20 @@ def generate_new_matrix(matrix):
                 matrix[line_index - 1][column_index - 1]
                 + matrix[(line_index + 1) % line_length][(column_index + 1) % columns_length]
                 + matrix[(line_index + 1) % line_length][column_index - 1]
-                + matrix[line_index  -1][(column_index + 1) % columns_length]
+                + matrix[line_index - 1][(column_index + 1) % columns_length]
             )
 
             neighbours = horizontal_neighbours + vertical_neighbours + diagonal_neighbours
             neighbours_size = len(neighbours.replace(' ', ''))
-            
-            if neighbours_size > 4:
-                result_matrix[line_index][column_index] = ' '
-            elif neighbours_size >= 2:
-                result_matrix[line_index][column_index] = '#'
-            else:
-                result_matrix[line_index][column_index] = ' '
 
-    
+            if neighbours_size == 2 and matrix[line_index][column_index] == "#":
+                result_matrix[line_index][column_index] = "#"
+            elif neighbours_size == 3:
+                result_matrix[line_index][column_index] = "#"
+            else:
+                result_matrix[line_index][column_index] = " "
+
+
     return result_matrix
 
 
